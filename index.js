8ab98faac96e6c31feaae4f45d1415aade95de68
jabss <jabrijuhinin93@gmail.com>
2024-02-12 23:34:00 +0800

    individual note can be displayed

--- index.js
+++ index.js
@@ -23,9 +23,18 @@ app.get('/', (request, response) => {
   response.send('<h1>Hello World!</h1>')
 })
 
-app.get('/api/notes', (request, response) => {
-  response.json(notes)
-  console.log("typeof notes",typeof notes)
+app.get('/api/notes/:id', (request, response) => {
+  const id = Number(request.params.id);
+  const note = notes.find(note => {
+    console.log(note.id, typeof note.id, id, typeof id, note.id === id)
+    return note.id === id
+  })
+ 
+  if (note) {
+    response.json(note)
+  } else {
+    response.status(404).end()
+  }
 })
 
 const PORT = 3001
