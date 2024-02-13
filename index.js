462bacfde11ab1b9c23d2c422131840f6cad90f9
jabss <jabrijuhinin93@gmail.com>
2024-02-13 23:14:01 +0800

    add new note now working

--- index.js
+++ index.js
@@ -41,9 +41,28 @@ app.get('/api/notes/:id', (request, response) => {
   }
 })
 
+const generateId = () => {
+  const maxId = notes.length > 0 ? 
+    Math.max(...notes.map(note=>note.id)) : 0
+  return maxId + 1;
+}
+
 app.post('/api/notes', (request, response) => {
-  const note = request.body
-  console.log(note)
+  const body = request.body
+  
+  if (!body.content) {
+    return response.status(400).json({
+      error: 'content missing'
+    })
+  }
+
+  const note = {
+    content: body.content,
+    important: Boolean(body.important) || false,
+    id : generateId(),
+  }
+
+  notes = notes.concat(note)
   response.json(note)
 })
 
