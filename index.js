33b4be576aa9b86b140e1dcd3ad68c1a2de16960
jabss <jabrijuhinin93@gmail.com>
2024-02-16 00:19:07 +0800

    change port

--- index.js
+++ index.js
@@ -75,7 +75,7 @@ app.delete('/api/notes/:id', (request, response) => {
   response.status(204).end()
 })
 
-const PORT = 3001
+const PORT = 3002
 app.listen(PORT, () => {
   console.log(`Server running on port ${PORT}`)
 })
