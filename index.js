751fd7eeafef9ebc44209e24678ea6431fe7c9fa
jabss <jabrijuhinin93@gmail.com>
2024-06-14 23:10:50 +0800

    update 'findByIdAndUpdate' in PUT method

--- index.js
+++ index.js
@@ -134,7 +134,11 @@ app.put('/api/notes/:id', (request, response, next) => {
     important: body.important,
   }
 
-  Note.findByIdAndUpdate(request.params.id, note, { new: true })
+  Note.findByIdAndUpdate(
+    request.params.id, 
+    note,
+    { new: true, runValidators: true, context: 'query' }
+  )
     .then(updatedNote => {
       response.json(updatedNote)
     })
