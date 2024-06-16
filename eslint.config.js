9868356e3c7ac0bc74f3c7e61d1ddc7cc0a24029
jabss <jabrijuhinin93@gmail.com>
2024-06-16 18:42:20 +0800

    added and configured eslint

---
+++ eslint.config.js
@@ -0,0 +1,32 @@
+const globals = require('globals');
+const stylisticJsPlugin = require('@stylistic/eslint-plugin-js');
+
+// Manually specify recommended rules (a simplified example)
+const recommendedRules = {
+  // Add other recommended rules here
+  'eqeqeq': 'error',
+  'no-trailing-spaces': 'error',
+  'object-curly-spacing': ['error', 'always'],
+  'arrow-spacing': ['error', { 'before': true, 'after': true }],
+  'no-console': 0
+};
+
+module.exports = [
+  {
+    files: ["**/*.js"],
+    languageOptions: {
+      sourceType: "commonjs",
+      globals: globals.browser,
+    },
+    plugins: {
+      '@stylistic/js': stylisticJsPlugin,
+    },
+    rules: {
+      ...recommendedRules,
+      '@stylistic/js/indent': ['error', 2],
+      '@stylistic/js/linebreak-style': ['error', 'unix'],
+      '@stylistic/js/quotes': ['error', 'single'],
+      '@stylistic/js/semi': ['error', 'never'],
+    },
+  },
+];
