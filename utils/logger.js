979a3d5e0d2640760522ecaa5ffdf37e93ae2931
jabss <jabrijuhinin93@gmail.com>
2024-06-17 01:12:04 +0800

    update logger module

---
+++ utils/logger.js
@@ -0,0 +1,22 @@
+// Info
+
+const info = (...params) => {
+    console.log(...params)
+  }
+// Error
+
+const error = (error, request, response, next) => {
+    console.error(error.message)
+  
+    if (error.name === 'CastError') {
+      return response.status(400).send({ error: 'malformatted id' })
+    } else if (error.name === 'ValidationError') {
+      return response.status(400).json({ error: error.message })
+    }
+  
+    next(error)
+  }
+
+module.exports = {
+    info, error
+}
