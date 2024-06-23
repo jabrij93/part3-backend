46289debba9dd0ec1f639741dca61e7f2614ce79
jabss <jabrijuhinin93@gmail.com>
2024-06-23 22:14:49 +0800

    add 2 new tests - when new notes added, when new notes without content added

--- tests/note_api.test.js
+++ tests/note_api.test.js
@@ -18,16 +18,15 @@ const initialNotes = [
     },
   ]
 
-test.only('notes are returned as json', async () => {
+test('notes are returned as json', async () => {
     await api
       .get('/api/notes')
       .expect(200)
       .expect('Content-Type', /application\/json/)
 })
 
-test.only('there are two notes', async () => {
+test('there are two notes', async () => {
     const response = await api.get('/api/notes')
-  
     assert.strictEqual(response.body.length, initialNotes.length)
 })
   
@@ -36,6 +35,42 @@ test('the first note is about HTTP methods', async () => {
   
     const contents = response.body.map(e => e.content)
     assert(contents.includes('HTML is easy'))
+})
+
+test('a valid note can be added ', async () => {
+  const newNote = {
+    content: 'async/await simplifies making async calls',
+    important: true,
+  }
+
+  await api
+    .post('/api/notes')
+    .send(newNote)
+    .expect(201)
+    .expect('Content-Type', /application\/json/)
+
+  const response = await api.get('/api/notes')
+
+  const contents = response.body.map(r => r.content)
+
+  assert.strictEqual(response.body.length, initialNotes.length + 1)
+
+  assert(contents.includes('async/await simplifies making async calls'))
+})
+
+test.only('note without content is not added', async () => {
+  const newNote = {
+    important: true
+  }
+
+  await api
+    .post('/api/notes')
+    .send(newNote)
+    .expect(400)
+
+  const response = await api.get('/api/notes')
+
+  assert.strictEqual(response.body.length, initialNotes.length)
 })
 
 beforeEach(async () => {
