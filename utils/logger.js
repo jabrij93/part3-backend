054e0c3938058dee6c6fc2238aeb66dea50b1f59
jabss <jabrijuhinin93@gmail.com>
2024-08-14 03:14:59 +0800

    display log even when in testing environment

--- utils/logger.js
+++ utils/logger.js
@@ -1,14 +1,23 @@
 const info = (...params) => {
-  if (process.env.NODE_ENV !== 'test') { 
+  if (process.env.NODE_ENV === 'start:test') {
+    console.log("## TEST MODE ##")
     console.log(...params)
-  }
+    console.log("## TEST MODE ##")
+} else {
+  console.log(...params)
+}
 }
 
 const error = (...params) => {
-  if (process.env.NODE_ENV !== 'test') { 
+  if (process.env.NODE_ENV === 'start:test') { 
+    console.log("## TEST MODE ##")
     console.error(...params)
-  }
+    console.log("## TEST MODE ##")
+  } else {
+    console.error(...params)
+    }
 }
+
 module.exports = {
     info, error
 }
