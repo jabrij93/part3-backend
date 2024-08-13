eafc271c5d9a23950fcd4d1c081037b2dcc4a279
jabss <jabrijuhinin93@gmail.com>
2024-08-14 03:23:43 +0800

    display logs in either test, dev or prod environment

--- utils/logger.js
+++ utils/logger.js
@@ -1,21 +1,9 @@
 const info = (...params) => {
-  if (process.env.NODE_ENV === 'start:test') {
-    console.log("## TEST MODE ##")
-    console.log(...params)
-    console.log("## TEST MODE ##")
-} else {
   console.log(...params)
-}
 }
 
 const error = (...params) => {
-  if (process.env.NODE_ENV === 'start:test') { 
-    console.log("## TEST MODE ##")
-    console.error(...params)
-    console.log("## TEST MODE ##")
-  } else {
-    console.error(...params)
-    }
+  console.log(...params)
 }
 
 module.exports = {
