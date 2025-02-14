6b67bd6858e17d0fae69df9d96875e54bcfcff9f
jabss <jabrijuhinin93@gmail.com>
2025-02-15 00:41:27 +0400

    remove and reinstall dependencies

--- app.js
+++ app.js
@@ -26,7 +26,7 @@ app.use(cors())
 app.use(express.static('dist'))
 app.use(express.json())
 app.use(middleware.requestLogger)
-  
+
 app.use('/api/notes', notesRouter)
 app.use('/api/users', usersRouter)
 app.use('/api/login', loginRouter)
@@ -35,8 +35,8 @@ if (process.env.NODE_ENV === 'test' || process.env.NODE_ENV === 'start:test') {
   const testingRouter = require('./controllers/testing')
   app.use('/api/testing', testingRouter)
 }
-  
+
 app.use(middleware.unknownEndpoint)
 app.use(middleware.errorHandler)
-  
+
 module.exports = app
